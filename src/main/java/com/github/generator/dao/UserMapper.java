26880ec107318be9fabb163a2994a6baaa9e8414
imByteCat <imbytecat@gmail.com>
2020-02-02 14:01:34 +0800

    init

---
+++ src/main/java/com/github/generator/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.github.generator.dao;
+
+import com.github.generator.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
